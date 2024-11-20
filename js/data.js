3b9a42e55c1a9fa61b1164f8b61ea9f71be65090
avrilstihler <aavril.stihler@gmail.com>
2024-11-20 14:32:09 -0300

    Update data.js

--- js/data.js
+++ js/data.js
@@ -4,7 +4,7 @@ export const data = [
    {
       title: 'Shingeki no Kyojin',
       description: ' Uma história complexa sobre humanidade, sacrifício e liberdade, "Shingeki no Kyojin" nos transporta para um mundo devastado por titãs, onde os protagonistas enfrentam forças além de sua compreensão. A série explora não apenas a luta pela sobrevivência, mas também os dilemas morais, os efeitos do poder e a natureza das sociedades. A trama é marcada por reviravoltas e questionamentos filosóficos sobre o ciclo de violência, o sentido da guerra e a busca por um futuro melhor, tudo enquanto desafia as expectativas do público com suas profundas revelações.',
-      image: './imagens/shingekinokyojin.jpg',
+      image: 'https://avrilstihler.github.io/Obras-Favoritas/imagens/shingekinokyojin.jpg',
       alt: 'Pôster de Shingeki no Kyojin',
       tag: 'ação, fantasia, filosofia',
       
