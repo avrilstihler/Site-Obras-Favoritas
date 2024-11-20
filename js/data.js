e944c23448c7aca6789234046054a2ce1297bb95
avrilstihler <aavril.stihler@gmail.com>
2024-11-20 14:23:19 -0300

    Update data.js

--- js/data.js
+++ js/data.js
@@ -12,7 +12,7 @@ export const data = [
    {
       title: 'Sonny Boy', 
       description: 'Com uma narrativa surreal e introspectiva, "Sonny Boy" é uma obra que mistura mistério, ficção científica e drama psicológico. O anime segue um grupo de estudantes que, após serem transportados para uma dimensão paralela, precisam lidar com os próprios poderes e os conflitos internos que surgem entre eles. A série explora temas como a solidão, a identidade, o poder, e o impacto da liberdade sobre os seres humanos, tudo isso com um estilo artístico único e uma abordagem não linear que deixa o público refletindo sobre a natureza da existência.',
-      image: 'sonnyboy.jpg',
+      image: '/imagens/sonnyboy.jpg',
       alt: 'Pôster de Sonny Boy',
       tag: 'ficção científica, psicológico, surrealismo',
       
@@ -20,7 +20,7 @@ export const data = [
    {
       title: 'Koe no Katachi',
       description: 'Uma obra tocante e delicada sobre redenção, aceitação e as complexas relações humanas. "Koe no Katachi" (A Silent Voice) conta a história de Shoya Ishida, um jovem que, após ter intimidado uma colega surda, tenta se redimir anos depois. A narrativa aborda temas profundos como bullying, depressão e a luta pelo perdão, enquanto mergulha na importância da comunicação e da empatia. A animação e a música complementam a carga emocional, criando uma experiência cinematográfica que ressoa com o público de maneira visceral.',
-      image: 'koenokatachi.jpg',
+      image: '/imagens/koenokatachi.jpg',
       alt: 'Pôster de Koe no Katachi',
       tag: 'drama, romance, empatia',
      
@@ -28,7 +28,7 @@ export const data = [
    {
       title: 'Arcane',
       description: 'Baseada no universo de "League of Legends", "Arcane" é uma série animada que transcende a simples adaptação de jogo, oferecendo uma narrativa poderosa sobre classe social, ambição, e os vínculos entre irmãs. A trama se passa na cidade de Piltover e seus subterrâneos, revelando a ascensão de figuras como Vi e Jinx e os conflitos internos que as moldam. A animação é um espetáculo visualmente impressionante, com uma direção de arte única que mistura elementos de pintura e grafismo digital, enquanto a trama explora as complexidades da moralidade e do poder em um mundo caótico.',
-      image: 'imagens/arcane.jpg',
+      image: '/imagens/arcane.jpg',
       alt: 'Pôster de Arcane',
       tag: 'ação, fantasia, drama',
      
@@ -36,7 +36,7 @@ export const data = [
    {
       title: 'Dungeon Meshi',
       description: 'Uma série de fantasia única que mistura aventura e culinária de maneira inusitada. "Dungeon Meshi" acompanha um grupo de aventureiros que exploram masmorras e enfrentam monstros, enquanto buscam ingredientes raros para preparar pratos deliciosos. A obra vai além da simples comédia de fantasia, explorando temas como amizade, sobrevivência e a complexidade dos personagens, tudo enquanto desafia as convenções do gênero ao colocar a gastronomia no centro da narrativa.',
-      image: 'dungeonmeshi.jpg',
+      image: '/imagens/dungeonmeshi.jpg',
       alt: 'Pôster de Dungeon Meshi',
       tag: 'fantasia, culinária, aventura',
       
@@ -44,7 +44,7 @@ export const data = [
    {
       title: 'In This Corner of the World',
       description: 'Com uma abordagem sensível e profunda, "In This Corner of the World" retrata a vida de uma jovem durante a Segunda Guerra Mundial no Japão, oferecendo uma visão humanista e delicada da guerra. A protagonista, Suzu, precisa navegar pelas dificuldades da vida em tempos de guerra, enfrentando perdas e adaptando-se a mudanças drásticas. A animação se destaca pela sua arte suave e pela forma como equilibra a dor e a esperança, revelando a força do espírito humano em tempos de crise.',
-      image: 'inthiscornerofthworld.jpg',
+      image: '/imagens/inthiscornerofthworld.jpg',
       alt: 'Pôster de In This Corner of the World',
       tag: 'drama, histórico, guerra',
      
@@ -52,14 +52,14 @@ export const data = [
    {
       title: 'Todas as Obras do Studio Ghibli',
       description: 'O Studio Ghibli, sob a direção de mestres como Hayao Miyazaki e Isao Takahata, criou uma vasta gama de filmes que exploram temas como a natureza, a vida, e a complexidade humana, tudo dentro de um universo estético e narrativo único. De "Meu Amigo Totoro" a "A Viagem de Chihiro", os filmes do Ghibli são mais do que simples animações; são reflexões profundas sobre o nosso lugar no mundo e a maneira como nos conectamos uns com os outros e com a natureza. Cada obra é uma janela para um universo onde o belo e o sombrio coexistem, onde personagens simples enfrentam dilemas existenciais em mundos fantásticos.',
-      image: 'ghibli3.jpg',
+      image: '/imagens/ghibli3.jpg',
       alt: 'Pôster das obras do Studio Ghibli',
       tag: 'anime, fantasia, aventura',
      
    },
    {title: 'Omori',
       description: '"Omori" é uma obra de RPG que combina estética colorida com uma narrativa profundamente sombria, abordando questões de identidade, trauma e crescimento. O jogo segue Omori, um garoto que vive em um mundo de fantasia, mas logo se vê confrontado com os desafios de sua própria mente e os efeitos das escolhas passadas. A jogabilidade envolve exploração e batalhas, mas o que realmente se destaca é a maneira como a história lida com emoções complexas, utilizando uma mecânica de jogo única para representar os estados mentais dos personagens.',
-      image: 'omori1.jpg',
+      image: '/imagens/omori1.jpg',
       alt: 'Pôster de Omori',
       tag: 'RPG, psicológico, aventura',
       
@@ -67,7 +67,7 @@ export const data = [
    {
       title: 'The Last of Us (Game)',
       description: '"The Last of Us" é mais do que um jogo de sobrevivência pós-apocalíptico; é uma jornada emocional sobre os laços humanos e o custo da humanidade em um mundo devastado. A história segue Joel e Ellie enquanto eles tentam sobreviver em um Estados Unidos devastado pela infecção de um fungo mortal. A obra explora temas como paternidade, moralidade, e o que significa realmente viver em um mundo onde a morte e a traição são constantes. A narrativa profunda, a construção de personagens e a jogabilidade imersiva tornam "The Last of Us" uma experiência única e inesquecível.',
-      image: 'lastofus.jpg',
+      image: '/imagens/lastofus.jpg',
       alt: 'Pôster de The Last of Us',
       tag: 'aventura, drama, sobrevivência',
       
@@ -75,7 +75,7 @@ export const data = [
    {
       title: 'Monster (2023)',
       description: 'Em Monster (2023), Hirokazu Kore-eda mergulha na questão de quem é o verdadeiro "monstro". A história segue a jornada de uma criança, sua mãe e seu professor, cujas vidas se entrelaçam após um incidente envolvendo acusações de bullying e abuso. O filme questiona o que define uma pessoa como "boa" ou "má", explorando os impactos das percepções e das ações humanas sobre os outros. Através de múltiplos pontos de vista, o filme revela as camadas de complexidade de cada personagem, onde o conceito de monstros se distorce e se questiona: seriam as crianças inocentes ou as circunstâncias sociais e familiares que as moldam? Kore-eda usa sua sensibilidade habitual para revelar como traumas, expectativas e mal-entendidos podem transformar o que parece ser uma simples história de culpa em uma reflexão sobre a natureza humana, criando uma narrativa que nos obriga a repensar quem realmente é o monstro.',
-      image: 'monster2023_koreeda.jpg',
+      image: '/imagens/monster2023_koreeda.jpg',
       alt: 'Pôster de Monster (2023)',
       tag: 'drama, psicológico, reflexão',
       
